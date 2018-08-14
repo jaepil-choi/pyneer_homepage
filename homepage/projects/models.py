4d3105e150459e50ccea7dd6aa579f0eaec45899
yoonjeoung <oyoons95@gmail.com>
2018-08-14 20:29:05 +0900

    project_detail페이지 연결

--- homepage/projects/models.py
+++ homepage/projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,6 +7,30 @@ class Project(models.Model):
     project_title = models.CharField(max_length=200)
     project_text = models.TextField()
     project_date = models.DateField('Date Finished')
+    # 멤버 이름
+    project_member = models.TextField(default="No one")
+    # 기타정보
+    project_extra = models.TextField(default=" ")
+    # 년도-학기
+    project_year_semester = models.TextField()
+    # 년도
+    project_year = models.TextField()
+
+    def project_year_semester(self):
+        if self.project_date.month >= 1 & self.project_date.month <= 6:
+            semester = 1
+        elif self.project_date.month >= 7 & self.project_date.month <= 12:
+            semester = 2
+        else:
+            semester = 3
+        return str(self.project_date.year) + '-' + str(semester)
+
+    def project_year(self):
+        return str(self.project_date.year)
+
+    def publish(self):
+        self.project_date = timezone.now()
+        self.save()
 
     def __str__(self):
         return self.project_title
--- homepage/projects/views.py
+++ homepage/projects/views.py
@@ -21,43 +21,9 @@ class IndexView(generic.ListView):
         projects = Project.objects.order_by('project_date')
 
         context['years'] = [x for x in range(projects.first().project_date.year, projects.last().project_date.year + 1)][::-1]
-        # for y in range(projects.first().project_date.year, projects.last().project_date.year + 1):
-        #     context['project_{0}'.format(y)] = projects.filter(project_date__year=y)
-            # context['years'] = context['years'].append(y)
-        # for y in range(projects.first().project_date.year, projects.last().project_date.year + 1):
-            # et["projects_{0}".format(y)] = projects.filter(project_date__year=y)
-        # et['projects_2018'] = projects.filter(project_date__year=2018)
-        # et['projects_2017'] = projects.filter(project_date__year=2017)
         return context
-
-# def IndexView(request):
-#     projects = Project.objects.order_by('-project_date')
-#     projects_by_year = {}
-#     for y in range(projects.first().project_date.year, projects.last()project_date.year + 1):
-#         projects_by_year[y] = projects.filter(project_date__year=y)
-#     context = {
-#         'projects_by_year': projects_by_year
-#     }
-#     return render(request, 'projects/index-old.html', context)
-
 
 class DetailView(generic.DetailView):
     model = Project
-    template_name = 'projects/detail.html'
-
-#
-# def index(request):
-#     latest_project_list = Project.objects.order_by('-project_date')[:5]
-#     context = {
-#         'latest_project_list': latest_project_list,
-#     }
-#     return render(request, 'projects/index-old.html', context)
-#
-# def detail(request, project_id):
-#     project = get_object_or_404(Project, pk=project_id)
-#     context = {
-#         'project': project,
-#     }
-#     return render(request, 'projects/detail.html', )
-#
+    template_name = 'projects/detail_context.html'
 
