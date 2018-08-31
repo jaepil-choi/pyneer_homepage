b5502a1372112ca0ba35382dd000273f7117cf12
yoonjeoung <oyoons95@gmail.com>
2018-08-31 19:54:49 +0900

    edit static root

--- homepage/homepage/settings.py
+++ homepage/homepage/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/pyneer/pyneer_homepage/homepage/projects/static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
